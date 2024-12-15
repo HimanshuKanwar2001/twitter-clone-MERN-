f0610ca8a12d39eb0f96521f6c1c81cbfeb9d1ca
himanshukanwar2001 <himanshukanwar2001@gmail.com>
2024-12-15 22:56:38 +0530

    auth contollers added

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,20 @@ import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongodDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); //to parse req.body
+app.use(express.urlencoded({ extended: true })); //to parse form data(urlencoded)
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-  console.log("Server is running on port ",PORT);
+  console.log("Server is running on port ", PORT);
   connectMongoDB();
 });
