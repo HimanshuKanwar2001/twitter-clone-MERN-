f8af336792aab426c6559856b2145d1b94b59a6f
Himanshukanwar2001 <himanshukanwar2001@gmail.com>
2025-02-20 11:06:06 +0530

    added UI design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,24 @@ import { Route, Routes } from "react-router-dom";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
 import LoginPage from "./pages/auth/login/LoginPage";
 import HomePage from "./pages/home/HomePage";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+
 import Sidebar from "./components/common/Sidebar";
 import RightPanel from "./components/common/RightPanel";
 
 function App() {
   return (
     <div className="flex max-w-6xl mx-auto">
-      <Sidebar/>
+      <Sidebar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/profile/:username" element={<ProfilePage />} />
       </Routes>
-      <RightPanel/>
+      <RightPanel />
     </div>
   );
 }
